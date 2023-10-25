8f51daa25a17c3cbaa682ca6e2176258ad2db68d
emon5122 <emon5122@gmail.com>
2023-10-26 00:36:12 +0600

    countries

--- src/worker.ts
+++ src/worker.ts
@@ -1,18 +1,6 @@
+import { countries } from "@nexisltd/country";
 export default {
-    async fetch(
-        request: Request,
-        env: any,
-        ctx: ExecutionContext
-    ): Promise<Response> {
-        if (request.method !== "GET") {
-            const { name }: { name: string } = await request.json();
-            return new Response(
-                `Hey ${name}! Greetings from Cloudflare serverless.`
-            );
-        } else {
-            return new Response(
-                "Hey Unknown! Greetings from Cloudflare serverless."
-            );
-        }
+    async fetch(request: Request): Promise<Response> {
+        return Response.json({ countries: countries() });
     },
 };
